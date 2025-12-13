fe2c71adf34af180a572b02dff23d6a2f44e371d
mjohanbintangp <m.johanbintang.pratama@gmail.com>
2025-12-13 20:15:13 +0700

    first commit btw

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <main className="w-screen min-h-screen flex justify-center items-center font-[poppins]">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-medium">Hello world</h1>
+        <a className="transition-all duration-300 hover:text-gray-300 font-medium text-md" target="_blank" href="https://mjohanbintangp.dev/">
+          mjohanbintangp.dev
+        </a>
+      </div>
+    </main>
+  );
+}
